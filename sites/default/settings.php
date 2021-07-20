18b5612da73724097789d6fd11ad7a77e0f0736c
mohamedabidi97 <abidi.contact@gmail.com>
2021-07-20 14:49:44 +0200

    custom template

---
+++ sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'Drupal_db',
+  'username' => 'drupal_user',
+  'password' => 'serbia2021',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'zY---HQ0uGMTuuWD3-5LhJ9Rli-VVjo1AoeFPQUOmKwr0m4pJLLwUr502zU9eEJpoJTtDG2A5g';
+$settings['config_sync_directory'] = 'sites/default/files/config_QbXgLm8PMwBs8JjAHqdMJA-H_g9q5B6ffA-6ZH3ntk-OTdL9UBPyDKS-7HZ_0RI3UT59B_qcsA/sync';
