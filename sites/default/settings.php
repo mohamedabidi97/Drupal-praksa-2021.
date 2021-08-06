bed57f05a47cb91e6f70257e5d59cec6a6240f78
Mohamed Abidi <70580695+mohamedabidi97@users.noreply.github.com>
2021-08-06 10:51:50 +0200

    Update the settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -10,4 +10,4 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 $settings['hash_salt'] = 'zY---HQ0uGMTuuWD3-5LhJ9Rli-VVjo1AoeFPQUOmKwr0m4pJLLwUr502zU9eEJpoJTtDG2A5g';
-$settings['config_sync_directory'] = 'sites/default/files/config_QbXgLm8PMwBs8JjAHqdMJA-H_g9q5B6ffA-6ZH3ntk-OTdL9UBPyDKS-7HZ_0RI3UT59B_qcsA/sync';
+$settings['config_sync_directory'] = 'var/sync';
