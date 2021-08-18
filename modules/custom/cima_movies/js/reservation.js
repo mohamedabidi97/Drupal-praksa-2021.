05ac9de64e13a7c2875b420eab2c27a32347b755
Mohamed Abidi <70580695+mohamedabidi97@users.noreply.github.com>
2021-08-18 15:35:45 +0200

    #22- Open popup on reserve movie button click

--- modules/custom/cima_movies/js/reservation.js
+++ modules/custom/cima_movies/js/reservation.js
@@ -2,21 +2,40 @@
 
     function editContent() {
       $('#listTable tr').click(function () {
-        if ($(this).hasClass('selected')) {
-          $('#btn').remove();
-          $('#res').remove();
-          $(this).removeClass('selected');
-          $(this).css('background', 'white');
-  
-        } else {
-          $('#listTable tr.selected').removeClass('selected');
-          $(this).addClass('selected');
-          $('#firstRow').append("<th id='res'>Reservation</th>")
-          $(this).append("<td id='btn'><button>Reserve</button></td>");
-          $(this).css('background', 'blue');
-        }
-      });
+        if (!$(e.target).is('button')) { // Make a difference between the click on the row and the button
+            if ($(this).hasClass('selected')) { 
+            $('#btn').remove();
+            $('#res').remove();
+            $(this).removeClass('selected');
+            $(this).css('background', 'white');
+    
+            } else {
+            $('#listTable tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            $('#firstRow').append("<th id='res'>Reservation</th>")
+            $(this).append("<td id='btn'><button>Reserve</button></td>");
+            $(this).css('background', 'blue');
+            }}
+        });
     }
+
+    function popup() {
+        let days = [];
+        $("#listTable,.modal").on('click', '.btn', function click(e) {
+          let currentRow = $(this).closest("tr");
+          let availableDays = currentRow.find("td:eq(4)").text().trim().replace(/ /g, '').split("\n"); // Remove spaces and Split the text to an array of days
+          let movieName = currentRow.find("td:eq(0)").text();
+          $('.days').empty();
+          $('#movieName').text(movieName);
+          $('.modal').toggleClass('is-visible');
+          $.each(availableDays, function (i, item) {
+            $('.days').append("<button class='available'>" + item + "</button>")
+          });
+          $(".available").click(function () {
+            $('.confirm').html("<button>Confirm your movie reservation</button>");
+          });
+        });
+      }
   
     let json = {};
     editContent();
